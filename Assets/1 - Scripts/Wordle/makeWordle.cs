8cd98f0447bd60d91693f3aceee05545b1f0d294
Mats <Matsdoornhein04@gmail.com>
2025-03-05 14:15:55 +0100

    Beginning of Wordle

---
+++ Assets/1 - Scripts/Wordle/makeWordle.cs
@@ -0,0 +1,23 @@
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeWordle : MonoBehaviour
+{  
+    void Start()
+    {
+        string Word = "Tests";
+        GenerateWordle(Word);
+    }
+
+    void GenerateWordle(string Word)
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
