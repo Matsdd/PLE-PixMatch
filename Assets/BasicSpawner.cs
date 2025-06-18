720bb88a31f88b2024a8ae77c811a4d546473816
Mats <53276381+Matsdd@users.noreply.github.com>
2025-06-18 09:21:14 +0200

    Multi fix?

--- Assets/BasicSpawner.cs
+++ Assets/BasicSpawner.cs
@@ -53,19 +53,19 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
         });
     }
     
-    // private void OnGUI()
-    // {
-    //     if (_runner == null)
-    //     {
-    //         if (GUI.Button(new Rect(0,0,200,40), "Host"))
-    //         {
-    //             StartGame(GameMode.Host);
-    //         }
-    //         if (GUI.Button(new Rect(0,40,200,40), "Join"))
-    //         {
-    //             StartGame(GameMode.Client);
-    //         }
-    //     }
-    // }
+    private void OnGUI()
+    {
+        if (_runner == null)
+        {
+            if (GUI.Button(new Rect(0,0,200,40), "Host"))
+            {
+                StartGame(GameMode.Host);
+            }
+            if (GUI.Button(new Rect(0,40,200,40), "Join"))
+            {
+                StartGame(GameMode.Client);
+            }
+        }
+    }
 }
 
