a18907b860d132d7d4275ed5c07ab3777563ce35
Mats <53276381+Matsdd@users.noreply.github.com>
2025-07-01 22:22:32 +0200

    9

--- Assets/1 - Scripts/Multiplayer/NetworkPlayer.cs
+++ Assets/1 - Scripts/Multiplayer/NetworkPlayer.cs
@@ -5,16 +5,27 @@ public class NetworkPlayer : NetworkBehaviour
 {
     [Networked] public NetworkString<_32> PlayerName { get; set; }
     [Networked] public int SkinIndex { get; set; }
+    
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_SetPlayerData(string name, int skin)
+    {
+        PlayerName = name;
+        SkinIndex = skin;
 
+        Debug.Log($"[Server] RPC received: Name = {PlayerName}, Skin = {SkinIndex}");
+    }
+    
     public override void Spawned()
     {
         if (HasInputAuthority)
         {
             string localName = PlayerPrefs.GetString("PlayerNameKey", "Player");
-            PlayerName = localName;
-            SkinIndex = PlayerPrefs.GetInt("PlayerSkinKey", 0);
+            int skin = PlayerPrefs.GetInt("PlayerSkinKey", 0);
 
-            Debug.Log($"Spawned NetworkPlayer with name: {PlayerName} skin: {SkinIndex}");
+            RPC_SetPlayerData(localName, skin);
+
+            Debug.Log($"[Client] Sent RPC to set name = {localName}, skin = {skin}");
         }
     }
+
 }
