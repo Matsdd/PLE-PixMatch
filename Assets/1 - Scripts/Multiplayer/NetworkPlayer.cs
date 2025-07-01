841e3f6a14a2175059c96bfe5a1af2968d621b95
Mats <53276381+Matsdd@users.noreply.github.com>
2025-07-02 01:14:02 +0200

    currentSkinIndex

--- Assets/1 - Scripts/Multiplayer/NetworkPlayer.cs
+++ Assets/1 - Scripts/Multiplayer/NetworkPlayer.cs
@@ -13,7 +13,7 @@ public class NetworkPlayer : NetworkBehaviour
         {
             RPC_SetPlayerData(
                 PlayerPrefs.GetString("PlayerName", "Player"),
-                PlayerPrefs.GetInt("SkinIndex", 0)
+                PlayerPrefs.GetInt("currentSkinIndex", 0)
             );
         }
     }
